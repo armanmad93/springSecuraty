9bb5a9428f63d6d3a15c6a3938a8059f0b1b1ea6
armanmad93 <armanmad93@gmail.com>
2021-07-20 20:45:44 +0400

    Initial commit

---
+++ src/test/java/com/example/springsecuraty/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuraty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
